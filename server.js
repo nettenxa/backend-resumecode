f56c8645c043bf9c415f3e68cdb31bef24ab5bb9
Atibodee Mahawan <atibodee.mah@gmail.com>
2023-03-18 13:30:29 +0700

    test

--- server.js
+++ server.js
@@ -4,16 +4,16 @@ const bodyParser = require("body-parser");
 const passport = require("passport");
 
 const users = require("./routes/api/users");
-const teams = require('./routes/api/items');
-const fileRoutes = require('./routes/file-upload-routes');
-const {PythonShell} =require('python-shell');
+const teams = require("./routes/api/items");
+const fileRoutes = require("./routes/file-upload-routes");
+const { PythonShell } = require("python-shell");
 
 const app = express();
 
 // Bodyparser middleware
 app.use(
   bodyParser.urlencoded({
-    extended: false
+    extended: false,
   })
 );
 app.use(bodyParser.json());
@@ -23,12 +23,9 @@ const db = require("./config/keys").mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    db,
-    { useNewUrlParser: true }
-  )
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+  .catch((err) => console.log(err));
 
 // Passport middleware
 app.use(passport.initialize());
@@ -36,31 +33,34 @@ app.use(passport.initialize());
 // Passport config
 require("./config/passport")(passport);
 
-app.post("/predict", (req, res, next)=>{
+app.post("/predict", (req, res, next) => {
   //Here are the option object in which arguments can be passed for the python_test.js.
   var input = {
-      "win": req.body.win,
-      "lose": req.body.lose
-  }
-  console.log(input.win," VS ",input.lose)
+    win: req.body.win,
+    lose: req.body.lose,
+  };
+  console.log(input.win, " VS ", input.lose);
   let options = {
-      args: [input.win, input.lose]
-  }
-   
-  PythonShell.run('app.py', options, function (err, result) {
-        if (err) throw err;
-        // result is an array consisting of messages collected
-        //during execution of script.
-        console.log('result: ', result.toString());
-        res.send(result.toString())
+    args: [input.win, input.lose],
+  };
+
+  PythonShell.run("app.py", options, function (err, result) {
+    if (err) throw err;
+    // result is an array consisting of messages collected
+    //during execution of script.
+    console.log("result: ", result.toString());
+    res.send(result.toString());
   });
 });
 
-
-
 // Routes
 app.use("/api/users", users);
-app.use('/api/teams', teams);
+app.use("/api/teams", teams);
+
+app.get("/", (req, res) => {
+  console.log("NetteN Hello World!!");
+  res.send("NetteN Hello World!!");
+});
 // app.use('/api', fileRoutes.routes);
 
 const port = process.env.PORT || 5000;
