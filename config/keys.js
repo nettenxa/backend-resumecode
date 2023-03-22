9df6a89c4dcf9b3fba72bd958d75212420cfde59
Atibodee Mahawan <atibodee.mah@gmail.com>
2023-03-22 09:26:44 +0700

    ok

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://netten:netten@cluster0.h1zfh3l.mongodb.net/?retryWrites=true&w=majority",
+    mongoURI: "mongodb+srv://netten:netten@cluster0.h1zfh3l.mongodb.net/test",
     secretOrKey: "secret"
   };
