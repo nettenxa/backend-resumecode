a5fe1cca248eff7866100002ae66ca449cdae67d
Atibodee Mahawan <atibodee.mah@gmail.com>
2023-03-27 09:42:25 +0700

    MONGO_URI

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://netten:netten@cluster0.h1zfh3l.mongodb.net/test",
+    MONGO_URI: "mongodb+srv://netten:netten@cluster0.h1zfh3l.mongodb.net/test",
     secretOrKey: "secret"
   };
